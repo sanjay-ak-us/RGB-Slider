9c5303d9705eee83da9194b3c66d2afbd57cedb9
sanjay-ak-us <sanjay.ak.us@gmail.com>
2015-01-06 12:16:51 +0100

    added navigation controller

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 //comment
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redSlider: UISlider!;
+    @IBOutlet weak var greenSlider: UISlider!;
+    @IBOutlet weak var blueSlider: UISlider!;
+    @IBOutlet weak var colorSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor;
+        colorSquare.layer.borderWidth = 1;
+        updateBackground();
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func updateBackground() {
+        colorSquare.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(1));
+    }
 }
 
