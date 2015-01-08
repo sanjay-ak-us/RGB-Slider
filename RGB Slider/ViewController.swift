3a6c32e767f2c441cb1be2bd2469bcef7a0e222f
sanjay-ak-us <sanjay.ak.us@gmail.com>
2015-01-08 10:42:51 +0100

    saving defaults

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -19,9 +19,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let defaults = NSUserDefaults.standardUserDefaults();
+        redSlider.value = defaults.floatForKey("red");
+        greenSlider.value = defaults.floatForKey("green");
+        blueSlider.value = defaults.floatForKey("blue");
         colorSquare.layer.borderColor = UIColor.blackColor().CGColor;
         colorSquare.layer.borderWidth = 1;
         updateBackground();
+        
 
     }
 
@@ -31,14 +36,23 @@ class ViewController: UIViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if(segue.identifier == "openColor"){
-            let newViewController =  segue.destinationViewController as UIViewController;
-            newViewController.view.backgroundColor = colorSquare.backgroundColor;
-        }
+        
+            if (segue.identifier == "openColor"){
+                let newViewController =  segue.destinationViewController as UIViewController;
+                newViewController.view.backgroundColor = colorSquare.backgroundColor;
+            }
+            if (segue.identifier == "openThird") {
+                let newViewController2 =  segue.destinationViewController as UIViewController;
+                newViewController2.view.backgroundColor = colorSquare.backgroundColor;
+            }
     }
 
     @IBAction func updateBackground() {
         colorSquare.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(1));
+        let defaults = NSUserDefaults.standardUserDefaults();
+        defaults.setFloat(redSlider.value, forKey:"red");
+        defaults.setFloat(greenSlider.value, forKey: "green");
+        defaults.setFloat(blueSlider.value, forKey: "blue");
     }
 }
 
